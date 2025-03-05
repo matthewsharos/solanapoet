e65c63fe578fab62a440bbdb7979a9a03dd36be0
Matthew Sharos <matthewsharos@gmail.com>
2025-03-05 12:04:41 -0600

    fix: remove unused resolve import

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
-import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
